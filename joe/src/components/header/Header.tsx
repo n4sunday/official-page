ab0b1bb0fe76537a1d3434d9cf52d6c0e6a07ac9
PIRDev <dev@pirsquare.net>
2020-10-13 03:11:11 +0700

    add header

--- joe/src/components/header/Header.tsx
+++ joe/src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Logo from "../Logo"
+import LogoWhite from "../LogoWhite"
 import styled from "styled-components"
 
 interface Props {
@@ -9,19 +10,57 @@ interface Props {
 }
 
 const StyledHeader = styled.header`
-  padding: 12px;
+  padding: 20px 12px;
 
   &.main-navbar {
     position: fixed;
     width: 100%;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     flex-direction: column;
     z-index: 9;
     transition: 0.3s all;
 
     .logo {
       display: block;
+      width: 180px;
+      margin: 0;
+    }
+
+    .right-menus {
+      display: flex;
+      align-items: center;
+      margin: 0 32px;
+
+      * {
+        color: #fff;
+        cursor: pointer;
+        transition: all 0.4s;
+        margin: 0 auto;
+
+        &:hover {
+          color: #c5283f;
+          transition: all 0.4s;
+        }
+      }
+
+      .menus {
+        display: grid;
+        grid-template-columns: 100px 100px 100px 100px;
+        align-items: center;
+      }
+
+      .lets-talk {
+        padding: 12px 32px;
+        margin: 0 24px;
+        border: 2px solid #fff;
+
+        &:hover {
+          border-color: #c5283f;
+          transition: all 0.4s;
+        }
+      }
     }
 
     @media screen and (min-width: 768px) {
@@ -29,34 +68,36 @@ const StyledHeader = styled.header`
 
       .logo {
         display: block;
-        width: 300px;
         transition: 0.3s all;
+        width: 300px;
+        margin: 0 32px;
       }
 
       &.sticky {
-        background: #fff;
+        background: #c5283f;
         box-shadow: -5px 14px 12px -15px rgba(0, 0, 0, 0.65);
         transition: 0.3s all;
 
         .logo {
-          width: 150px;
           transition: 0.3s all;
         }
 
-        .nav {
-          padding: 15px;
-          align-items: center;
-          display: flex;
-          transition: 0.3s all;
+        .right-menus {
+          * {
+            color: #fff;
+            cursor: pointer;
+            transition: all 0.4s;
 
-          ul.navbar {
-            margin-bottom: 0;
-            transition: 0.3s all;
+            &:hover {
+              color: #f26f82;
+              transition: all 0.4s;
+            }
+          }
 
-            li {
-              margin: 0 20px;
-              transition: 0.3s all;
-              padding-bottom: 0;
+          .lets-talk {
+            &:hover {
+              border-color: #f26f82;
+              transition: all 0.4s;
             }
           }
         }
@@ -71,8 +112,15 @@ export const Header = ({ sticky }: Props) => {
       className={`main-navbar ${sticky ? "sticky" : ""}`}
       id="main-nav"
     >
-      <div className="logo">
-        <Logo />
+      <div className="logo">{sticky ? <LogoWhite /> : <Logo />}</div>
+      <div className="right-menus">
+        <div className="menus">
+          <div>About</div>
+          <div>Services</div>
+          <div>Client</div>
+          <div>Showcase</div>
+        </div>
+        <div className="lets-talk">Let's talk</div>
       </div>
     </StyledHeader>
   )
