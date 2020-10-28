638fe589e46eb838499ffe53563e63899138e6e8
ihm <s5935512030@phuket.psu.ac.th>
2020-10-28 20:51:47 +0700

    pr#3

---
+++ air/src/components/structure/index.tsx
@@ -0,0 +1,26 @@
+import { h, FunctionalComponent } from "preact"
+import * as style from "./style.scss"
+import List from "./list"
+import { Typography } from "antd"
+import { Divider } from "antd"
+import Logo from "../../assets/images/structure.png";
+
+const { Text, Paragraph } = Typography
+const Structure: FunctionalComponent = () => (
+  <div className={style.container}>
+    <Text className={style.header}>Read what we write</Text>
+    <div className={style.wrapperList}>
+      <List />
+      <Divider className={style.divider} />
+      <List />
+      <Divider className={style.divider} />
+      <List />
+    </div>
+    <img src={Logo} className={style.img} alt=""/>
+    <Text underline className={style.underline}>
+      Browse our article
+    </Text>
+  </div>
+)
+
+export default Structure
