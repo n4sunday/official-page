91a41d91f947bffbfe402ffecf1afdfaf80913b4
ihm <s5935512030@phuket.psu.ac.th>
2020-10-29 00:27:43 +0700

    add font and aos

--- air/src/components/article/index.tsx
+++ air/src/components/article/index.tsx
@@ -8,21 +8,23 @@ import Logo from "../../assets/images/structure.png"
 const { Text, Paragraph } = Typography
 const Article: FunctionalComponent = () => (
   <div className={style.container}>
-    <Text className={style.header}>Read what we write</Text>
+    <Text className={style.header}  data-aos="fade-up" data-aos-duration="500">Read what we write</Text>
     <div className={style.wrapperList}>
-      <List />
-      <Divider className={style.divider} />
-      <List />
-      <Divider className={style.divider} />
+      <List  />
+      <Divider className={style.divider}  data-aos="fade-up" data-aos-duration="600"/>
+      <List  />
+      <Divider className={style.divider} data-aos="fade-up" data-aos-duration="700"/>
       <List />
     </div>
     <img
       src={Logo}
+      data-aos="fade-up"
       className={style.img}
       alt=""
       loading="lazy"
+      data-aos-duration="800"
     />
-    <Text underline className={style.underline}>
+    <Text underline className={style.underline}  data-aos="fade-up" data-aos-duration="900">
       Browse our article
     </Text>
   </div>
